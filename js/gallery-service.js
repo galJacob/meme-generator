78e8a8716650c019f6c245973b1e19681a3bd65d
galJacob <joojonogal2@gmail.com>
2018-06-20 14:43:27 +0300

    added img folder

--- js/gallery-service.js
+++ js/gallery-service.js
@@ -1,3 +1,18 @@
 'use strict';
 
 console.log('Gallery Service');
+var gImgs = [];
+
+function createImgs(){
+
+    
+}
+
+function createImg(){
+
+}
+
+var gImgs = [{id: 1, url: 'img/popo.jpg', keywords: ['happy']}];
+
+
+
