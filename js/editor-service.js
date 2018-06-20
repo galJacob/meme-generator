1a4d4d52c240533ca31c55e463585624840ebd9e
yedidic <yedidic@gmail.com>
2018-06-20 15:15:51 +0300

    Added html for the modal and some js

--- js/editor-service.js
+++ js/editor-service.js
@@ -1,3 +1,32 @@
 'use strict';
 
-console.log('Editors Service');
+console.log("Editor's Service");
+
+var gMeme = {
+  selectedImgId: 5,
+  txts: [
+    {
+      line: 'I never eat Falafel',
+      size: 20,
+      align: 'left',
+      color: 'red'
+    }
+  ]
+};
+setCanvas(gMeme);
+
+function setCanvas(meme) {
+  var elCanvas = document.querySelector('#canvas');
+  console.log(elCanvas);
+  var imgWidth;
+  var imgHeight;
+
+  var img = new Image();
+  img.onload = function() {
+    imgWidth = this.width;
+    imgHeight = this.height;
+    alert(this.width + 'x' + this.height);
+  };
+  img.src = '/img/';
+  // elCanvas.width =
+}
