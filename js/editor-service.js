4827cb0319c744c87877548bfe24627a1e0d35e4
yedidic <yedidic@gmail.com>
2018-06-20 17:30:59 +0300

    Added Modal

--- js/editor-service.js
+++ js/editor-service.js
@@ -14,29 +14,5 @@ var gMeme = {
   ]
 };
 
-setCanvas();
 
-function setCanvas() {
-  var elCanvas = document.querySelector('#canvas');
-  console.log(elCanvas);
 
-  var img = new Image();
-  img.onload = function() {
-    elCanvas.width = img.width;
-    elCanvas.height = img.height ;
-    var ctx = elCanvas.getContext('2d');
-    // ctx.fillStyle = 'whitesmoke';
-    // ctx.fillRect(0, 0, elCanvas.width, elCanvas.height);
-    ctx.drawImage(img, 0, 0);
-    renderTxtOnCanvas('Hello World');
-  };
-  img.src = `../meme-imgs/${gMeme.selectedImgId}.jpg`;
-}
-
-function renderTxtOnCanvas(txt, line = 100) {
-  var elCanvas = document.querySelector('#canvas');
-  var ctx = elCanvas.getContext('2d');
-  ctx.font = '30px Arial';
-  ctx.fillStyle = 'black';
-  ctx.fillText(txt, 45, line);
-}
--- js/main.js
+++ js/main.js
@@ -1,21 +1,57 @@
 'use strict';
 
 console.log(2);
-console.log('Didi the King!')
+console.log('Didi the King!');
 
-
-function renderGallery() {
-
-    var renderedGallery = gImgs.map(function (img) {
-        var strHtml = `<li onclick = "openModal(${img.id})" class="img-pick fit-background img-${img.id}" style="background-image: url(../meme-imgs/${img.id}.jpg)" </li>`;
-        return strHtml;
-    })
-    var renderedGallery = renderedGallery.join('');
-    var elImgsContainer = document.querySelector('.imgs-container ul');
-    console.log(renderedGallery);
-
-    elImgsContainer.innerHTML = renderedGallery;
-    // console.log(elImgsContainer);
+function init() {
+  renderGallery();
+  //   setCanvas();
 }
 
-renderGallery();
+function renderGallery() {
+  var renderedGallery = gImgs.map(function(img) {
+    var strHtml = `<li onclick = "openModal(${
+      img.id
+    })" class="img-pick fit-background img-${
+      img.id
+    }" style="background-image: url(../meme-imgs/${img.id}.jpg)" </li>`;
+    return strHtml;
+  });
+  var renderedGallery = renderedGallery.join('');
+  var elImgsContainer = document.querySelector('.imgs-container ul');
+  console.log(renderedGallery);
+
+  elImgsContainer.innerHTML = renderedGallery;
+  // console.log(elImgsContainer);
+}
+
+function setCanvas(id) {
+  var elCanvas = document.querySelector('#canvas');
+  console.log(elCanvas);
+
+  var img = new Image();
+  img.onload = function() {
+    elCanvas.width = img.width;
+    elCanvas.height = img.height;
+    var ctx = elCanvas.getContext('2d');
+    // ctx.fillStyle = 'whitesmoke';
+    // ctx.fillRect(0, 0, elCanvas.width, elCanvas.height);
+    ctx.drawImage(img, 0, 0);
+    renderTxtOnCanvas('Hello World');
+  };
+  img.src = `../meme-imgs/${id}.jpg`;
+}
+
+function renderTxtOnCanvas(txt, line = 100) {
+  var elCanvas = document.querySelector('#canvas');
+  var ctx = elCanvas.getContext('2d');
+  ctx.font = '30px Arial';
+  ctx.fillStyle = 'black';
+  ctx.fillText(txt, 45, line);
+}
+
+function openModal(id) {
+  var elModal = document.querySelector('.modal');
+  setCanvas(id);
+  elModal.style.display = 'block';
+}
