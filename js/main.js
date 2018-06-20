6e6f9ebffb6a90484194a602e6241bb7ded46f69
galJacob <joojonogal2@gmail.com>
2018-06-20 17:17:29 +0300

    fixed flex bug

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ console.log('Didi the King!')
 function renderGallery() {
 
     var renderedGallery = gImgs.map(function (img) {
-        var strHtml = `<li class="img-pick fit-background img-${img.id}" style="background-image: url(../meme-imgs/${img.id}.jpg)" </li>`;
+        var strHtml = `<li onclick = "openModal(${img.id})" class="img-pick fit-background img-${img.id}" style="background-image: url(../meme-imgs/${img.id}.jpg)" </li>`;
         return strHtml;
     })
     var renderedGallery = renderedGallery.join('');
