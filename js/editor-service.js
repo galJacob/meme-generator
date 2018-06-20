0fea5868e15c2e04ca23a5de26cf6d71818df511
yedidic <yedidic@gmail.com>
2018-06-20 16:41:07 +0300

    Added canvas and sample txt upon it

--- js/editor-service.js
+++ js/editor-service.js
@@ -13,20 +13,29 @@ var gMeme = {
     }
   ]
 };
-setCanvas(gMeme);
 
-function setCanvas(meme) {
+setCanvas();
+
+function setCanvas() {
   var elCanvas = document.querySelector('#canvas');
   console.log(elCanvas);
-  var imgWidth;
-  var imgHeight;
 
   var img = new Image();
   img.onload = function() {
-    imgWidth = this.width;
-    imgHeight = this.height;
-    alert(this.width + 'x' + this.height);
+    elCanvas.width = img.width + 20;
+    elCanvas.height = img.height + 20;
+    var ctx = elCanvas.getContext('2d');
+    ctx.fillStyle = 'whitesmoke';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 10, 10);
+    handleText('Hello World', ctx);
+    //Function that pushes fake txt
   };
-  img.src = '/img/';
-  // elCanvas.width =
+  img.src = `../meme-imgs/${gMeme.selectedImgId}.jpg`;
+}
+
+function handleText(txt, ctx, line = 100) {
+  ctx.font = '30px Arial';
+  ctx.fillStyle= 'black'
+  ctx.fillText(txt, 45, line);
 }
